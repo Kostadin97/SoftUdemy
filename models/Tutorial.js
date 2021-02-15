85a48f48037cf85242857fbdc727d8d25f66ca00
Kostadin <seimenski@icloud.com>
2021-02-15 22:30:37 +0200

    Create Tutorial Model

---
+++ models/Tutorial.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+
+const tutorialSchema = new mongoose.Schema({
+  id: mongoose.Types.ObjectId,
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  imageUrl: {
+    type: String,
+    required: true,
+  },
+  duration: {
+    type: String,
+    required: true,
+  },
+  creationDate: {
+    type: Date,
+    default: Date.now
+  },
+  enrolledUsers: [] 
+});
+
+module.exports = mongoose.model("Tutorial", tutorialSchema);
