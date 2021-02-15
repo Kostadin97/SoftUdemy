a739e12eaffc97b52b18166bed8071f0d0b109ec
Kostadin <seimenski@icloud.com>
2021-02-15 23:02:41 +0200

    Display Tutorials On Home Page

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,18 +1,25 @@
 const router = require("express").Router();
-const authMiddleware = require('../middlewares/authMiddleware');
-const tutorialService = require('../services/tutorialService');
+const authMiddleware = require("../middlewares/authMiddleware");
+const tutorialService = require("../services/tutorialService");
+const Handlebars = require("express-handlebars");
 
 router.get("/", authMiddleware.isGuest, (req, res) => {
-  res.render('homeUser');
+  tutorialService
+    .getAll()
+    .then((tutorials) => {
+      res.render("homeUser", { title: "Browse", tutorials, });
+    })
+    .catch(() => res.status(500).end());
 });
 
-router.get('/create', authMiddleware.isAuthenticated, (req, res) =>{
-  res.render('create', { title: 'Create Course' });
+router.get("/create", authMiddleware.isAuthenticated, (req, res) => {
+  res.render("create", { title: "Create Course" });
 });
 
-router.post('/create', (req, res) => {
-  tutorialService.create(req.body)
-    .then(() => res.redirect('/'))
+router.post("/create", (req, res) => {
+  tutorialService
+    .create(req.body)
+    .then(() => res.redirect("/"))
     .catch(() => res.status(500).end());
 });
 
--- services/tutorialService.js
+++ services/tutorialService.js
@@ -4,6 +4,13 @@ const create = (data) => {
     return new Tutorial(data).save();
 }
 
+async function getAll() {
+    let tutorials = await Tutorial.find({}).lean();
+
+    return tutorials;
+}
+
 module.exports = {
     create,
+    getAll,
 }
