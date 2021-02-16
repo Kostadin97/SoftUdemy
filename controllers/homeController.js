4dc60d8c4315dcbceb1f229170b7bcd24941293c
Kostadin <seimenski@icloud.com>
2021-02-16 20:00:46 +0200

    Add Edit Page and Search by query

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -2,17 +2,42 @@ const router = require("express").Router();
 const authMiddleware = require("../middlewares/authMiddleware");
 const tutorialService = require("../services/tutorialService");
 
-router.get("/", authMiddleware.isGuest, (req, res) => {
+router.get("/", (req, res) => {
   tutorialService
-    .getAll()
+    .getAll(req.query)
     .then((tutorials) => {
       res.render("homeUser", { title: "Browse", tutorials });
     })
-    .catch(() => res.status(500).end());
+    .catch((err) => console.log(err));
 });
 
 router.get("/create", authMiddleware.isAuthenticated, (req, res) => {
   res.render("create", { title: "Create Course" });
+});
+
+router.get("/details/:tutorialId", async (req, res) => {
+  let tutorial = await tutorialService.getOne(req.params.tutorialId);
+  res.render("details", { title: "Details Page", tutorial });
+});
+
+router.get("/delete/:tutorialId", (req, res) => {
+  tutorialService
+    .deleteTutorial(req.params.tutorialId)
+    .then(() => res.redirect("/"))
+    .catch(() => res.status(500).end());
+});
+
+router.get("/edit/:tutorialId", async (req, res) => {
+  let tutorial = await tutorialService.getOne(req.params.tutorialId);
+  res.render("edit", { title: "Edit Tutorial", tutorial });
+});
+
+router.post("/edit/:tutorialId", (req, res) => {
+  let courseId = req.params.tutorialId;
+  tutorialService
+    .editTutorial(courseId, req.body)
+    .then(() => res.redirect(`/details/${courseId}`))
+    .catch(() => res.status(500).end());
 });
 
 router.post("/create", (req, res) => {
--- services/tutorialService.js
+++ services/tutorialService.js
@@ -4,13 +4,32 @@ const create = (data) => {
   return new Tutorial(data).save();
 };
 
-async function getAll() {
+async function getAll(query) {
   let tutorials = await Tutorial.find({}).lean();
 
+  if (query.search) {
+    tutorials = tutorials.filter(x => x.title.toLowerCase().includes(query.search));
+}
+
   return tutorials;
+}
+
+function getOne(id) {
+  return Tutorial.findById(id).lean();  
+}
+
+function deleteTutorial(id) {
+  return Tutorial.findByIdAndDelete(id);
+}
+
+function editTutorial(id, data) {
+  return Tutorial.findByIdAndUpdate(id, data);
 }
 
 module.exports = {
   create,
   getAll,
+  getOne,
+  deleteTutorial,
+  editTutorial
 };
